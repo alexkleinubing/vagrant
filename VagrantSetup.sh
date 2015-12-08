9bbb06853afdefe32f26c46e3eb32461d842a62b
Alex K <alex.kleinubing@gmail.com>
2015-12-08 16:32:39 -0300

    Create VagrantSetup.sh

---
+++ VagrantSetup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "##### Instalando ambiente de Desenvolvimento PHP"
+
+echo "- Atualizando lista de pacotes"
+sudo apt-get update
+
+echo "- Definindo Senha padrão para o MySQL e suas ferramentas"
+DEFAULTPASS="12345678"
+sudo debconf-set-selections <<EOF
+mysql-server	mysql-server/root_password password $DEFAULTPASS
+mysql-server	mysql-server/root_password_again password $DEFAULTPASS
+dbconfig-common	dbconfig-common/mysql/app-pass password $DEFAULTPASS
+dbconfig-common	dbconfig-common/mysql/admin-pass password $DEFAULTPASS
+dbconfig-common	dbconfig-common/password-confirm password $DEFAULTPASS
+dbconfig-common	dbconfig-common/app-password-confirm password $DEFAULTPASS
+phpmyadmin		phpmyadmin/reconfigure-webserver multiselect apache2
+phpmyadmin		phpmyadmin/dbconfig-install boolean true
+phpmyadmin      phpmyadmin/app-password-confirm password $DEFAULTPASS 
+phpmyadmin      phpmyadmin/mysql/admin-pass     password $DEFAULTPASS
+phpmyadmin      phpmyadmin/password-confirm     password $DEFAULTPASS
+phpmyadmin      phpmyadmin/setup-password       password $DEFAULTPASS
+phpmyadmin      phpmyadmin/mysql/app-pass       password $DEFAULTPASS
+EOF
+
+echo "- Instalando pacotes básicos"
+sudo apt-get install vim curl python-software-properties git-core --assume-yes --force-yes
+
+echo "- Adicionando repositório do pacote PHP 5.6"
+sudo add-apt-repository ppa:ondrej/php5-5.6
+
+echo "- Atualizando lista de pacotes"
+sudo apt-get update
+
+echo "- Instalando MySQL, PHPMyAdmin e alguns outros módulos"
+sudo apt-get install mysql-server-5.5 mysql-client phpmyadmin --assume-yes --force-yes
+
+echo "- Instalando PHP, Apache e alguns módulos"
+sudo apt-get install php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt  php5-mysql --assume-yes --force-yes
+
+echo "- Habilitando mod-rewrite do Apache"
+sudo a2enmod rewrite
+
+echo "- Reiniciando Apache"
+sudo service apache2 restart
+
+echo "- Baixando e instalando Composer"
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+echo "- Instalando banco NoSQL Redis" 
+sudo apt-get install redis-server --assume-yes
+sudo apt-get install php5-redis
+
+# Instale a partir daqui o que você desejar 
+
+echo "[OK] Ambiente de desenvolvimento concluído"
