6d84ca61f35aaca2e43c941161c0fd6b7e835a6a
Alex K <alex.kleinubing@gmail.com>
2016-02-05 15:46:25 -0300

    Added Ruby, RVM and NodeJS fix provision

--- provision/vagrant-setup.sh
+++ provision/vagrant-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "- Atualizando lista de pacotes"
-sudo apt-get update
+sudo apt-get update >> /dev/null
 
 echo "- Definindo Senha padrão para o MySQL e suas ferramentas"
 DEFAULTPASS="12345678"
@@ -22,19 +22,20 @@ phpmyadmin      phpmyadmin/mysql/app-pass       password $DEFAULTPASS
 EOF
 
 echo "- Instalando pacotes básicos"
-sudo apt-get install vim joe curl python-software-properties git-core --assume-yes --force-yes
+sudo apt-get install vim joe curl python-software-properties git-core --assume-yes --force-yes >> /dev/null
+
 
 echo "- Adicionando repositório do pacote PHP 5.6"
-sudo add-apt-repository ppa:ondrej/php5-5.6
+sudo add-apt-repository ppa:ondrej/php5-5.6 >> /dev/null
 
 echo "- Atualizando lista de pacotes"
-sudo apt-get update
+sudo apt-get update >> /dev/null
 
 echo "- Instalando MySQL, PHPMyAdmin e alguns outros módulos"
-sudo apt-get install mysql-server-5.5 mysql-client phpmyadmin --assume-yes --force-yes
+sudo apt-get install mysql-server-5.5 mysql-client phpmyadmin --assume-yes --force-yes >> /dev/null
 
 echo "- Instalando PHP, Apache e alguns módulos"
-sudo apt-get install php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-intl php5-sqlite php5-mysql --assume-yes --force-yes
+sudo apt-get install php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-intl php5-sqlite php5-mysql --assume-yes --force-yes >> /dev/null
 
 echo "- Ajustando ServerName localhost"
 sudo echo 'ServerName localhost' >> /etc/apache2/apache2.conf
@@ -46,33 +47,30 @@ echo "- Reiniciando Apache"
 sudo service apache2 restart
 
 echo "- Baixando e instalando vhosts.sh"
+cd /var/www/
 git clone https://github.com/alexkleinubing/vhosts.git
 sudo chmod +x vhosts/vhost.sh
 sudo mv vhosts/vhost.sh /usr/bin/vhost
 
 echo "- Baixando e instalando Composer"
-curl -sS https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | php >> /dev/null
 sudo mv composer.phar /usr/local/bin/composer
 
 echo "- Instalando banco NoSQL Redis" 
-sudo apt-get install redis-server --assume-yes
-sudo apt-get install php5-redis
+sudo apt-get install redis-server --assume-yes >> /dev/null
+sudo apt-get install php5-redis >> /dev/null
 
 echo "- Instalando RVM e Ruby 2.3.0"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0
-rvm --default set 2.3.0
-
-echo "- Instalando RubyGems"
-wget https://rubygems.org/rubygems/rubygems-2.5.1.tgz
-tar xvf rubygems-2.5.1.tgz
-cd rubygems-2.5.1
-sudo ruby setup.rb
-gem update --system 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 >> /dev/null
+\curl -sSL https://get.rvm.io | bash -s stable >> /dev/null
+source ~/.rvm/scripts/rvm >> /dev/null
+rvm requirements >> /dev/null
+rvm install 2.3.0 >> /dev/null
+rvm use 2.3.0 --default >> /dev/null
 
 echo "- Instalando NodeJS"
-sudo apt-get install nodejs --assume-yes
-sudo apt-get install npm --assume-yes
+sudo apt-get install nodejs --assume-yes >> /dev/null
+sudo apt-get install npm --assume-yes >> /dev/null
 
 # Instale a partir daqui o que você desejar 
 
